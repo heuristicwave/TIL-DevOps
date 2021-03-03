a8b4b2c6262477b3dd3a8c7bf47e0eacb9ac3666
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-03-03 18:49:10 +0900

    edit resource name

--- IaC/Terraform/code/EKS/outputs.tf
+++ IaC/Terraform/code/EKS/outputs.tf
@@ -4,8 +4,6 @@
 
 locals {
   config_map_aws_auth = <<CONFIGMAPAWSAUTH
-
-
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -13,21 +11,20 @@ metadata:
   namespace: kube-system
 data:
   mapRoles: |
-    - rolearn: ${aws_iam_role.demo-node.arn}
+    - rolearn: ${aws_iam_role.eks-node.arn}
       username: system:node:{{EC2PrivateDNSName}}
       groups:
         - system:bootstrappers
         - system:nodes
 CONFIGMAPAWSAUTH
-
-  kubeconfig = <<KUBECONFIG
+  kubeconfig          = <<KUBECONFIG
 
 
 apiVersion: v1
 clusters:
 - cluster:
-    server: ${aws_eks_cluster.demo.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.demo.certificate_authority[0].data}
+    server: ${aws_eks_cluster.eks_cluster.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.eks_cluster.certificate_authority[0].data}
   name: kubernetes
 contexts:
 - context:
--- IaC/Terraform/code/EKS/vpc.tf
+++ IaC/Terraform/code/EKS/vpc.tf
@@ -6,49 +6,49 @@
 #  * Route Table
 #
 
-resource "aws_vpc" "demo" {
+resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = map(
-    "Name", "terraform-eks-demo-node",
+    "Name", "terraform-eks-node",
     "kubernetes.io/cluster/${var.cluster-name}", "shared",
   )
 }
 
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "eks_subnet" {
   count = 2
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
-  vpc_id                  = aws_vpc.demo.id
+  vpc_id                  = aws_vpc.eks_vpc.id
 
   tags = map(
-    "Name", "terraform-eks-demo-node",
+    "Name", "terraform-eks-node",
     "kubernetes.io/cluster/${var.cluster-name}", "shared",
   )
 }
 
-resource "aws_internet_gateway" "demo" {
-  vpc_id = aws_vpc.demo.id
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   tags = {
-    Name = "terraform-eks-demo"
+    Name = "terraform-eks"
   }
 }
 
-resource "aws_route_table" "demo" {
-  vpc_id = aws_vpc.demo.id
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo.id
+    gateway_id = aws_internet_gateway.eks_igw.id
   }
 }
 
-resource "aws_route_table_association" "demo" {
+resource "aws_route_table_association" "eks_association" {
   count = 2
 
-  subnet_id      = aws_subnet.demo.*.id[count.index]
-  route_table_id = aws_route_table.demo.id
+  subnet_id      = aws_subnet.eks_subnet.*.id[count.index]
+  route_table_id = aws_route_table.eks_route_table.id
 }

862b5ebb92e33a86ca831bce7d9987913282559e
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-09-06 16:14:45 +0900

    Create iam.tf

---
+++ IaC/Terraform/code/EKS/iam.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_policy" "cluster_autoscaler_policy" {
+  name        = "AmazonEKSClusterAutoscalerPolicy"
+  path        = "/"
+  description = "cluster-autoscaler-policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
